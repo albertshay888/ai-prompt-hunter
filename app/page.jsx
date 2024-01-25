8577b8e3dbf59ec77d5dfc0b7fbcb6fcf2452b4e
Dev Account <20329123+albertshay888@users.noreply.github.com>
2024-01-25 20:56:11 +0800

    inital commit

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import Feed from '@components/Feed';
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Explore & Share
+           <br />
+            <span className="blue_gradient
+            text-center">ChatGPT Prompts</span>
+        </h1>
+        <p className="desc text-center">
+          Introducing PromptHunter, an innovative open-source tool designed for the contemporary world,
+          empowering users to explore, generate, and collaborate on imaginative prompts.
+        </p>
+        <Feed />
+    </section>
+  )
+}
+
+export default Home;
